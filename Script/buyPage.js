4be8834ae48db7e4ea45e6441216a1138f4f5468
Wags15 <58407467+Wags15@users.noreply.github.com>
2021-02-18 21:40:26 -0500

    Updated buy page to get descition

--- Script/buyPage.js
+++ Script/buyPage.js
@@ -1,10 +1,9 @@
 function hover() {
     var image = document.getElementById('buyImg');
-    image.src = "..\\Images/error.jpg";
+    image.src = "..\\Images/error.jpg";     // By defualt changes to error image when hovering. change to proper one    
 }
 
 function unhover() {
-
     var img__name = localStorage.getItem("image");
     var image = document.getElementById('buyImg');
     image.src = img__name;
@@ -12,10 +11,14 @@ function unhover() {
 
 
 
-function getImage() {
-    var img__name = localStorage.getItem("image");
-    var name = localStorage.getItem("name");
-    var description = localStorage.getItem("description");
+function setInfo() {
+    var img__name = localStorage.getItem("image");  // Saves the image name (directory)
+    var name = localStorage.getItem("name");    // saves the image title
+    var description = localStorage.getItem("description"); //Saves the image decription
+
     var image = document.getElementById('buyImg');
-    image.src = img__name;
+    image.src = img__name;  // saves the image source to the proper one saved when the page is loaded
+
+    document.getElementById("title__text").innerHTML = name; //Sets the title sent by clicking picture
+    document.getElementById("description__text").innerHTML = description; //Sets the description sent by clicking picture
 }
